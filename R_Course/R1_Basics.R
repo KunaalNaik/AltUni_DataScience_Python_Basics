8db23d0d1a9ab8eec444042165c1a41eaca84b99
Kunaal Naik <33804703+KunaalNaik@users.noreply.github.com>
2023-10-07 19:36:59 +0530

    summary

--- R_Course/R1_Basics.R
+++ R_Course/R1_Basics.R
@@ -10,3 +10,29 @@ library(corrplot)
 
 #Data Import
 data <- read.csv("https://raw.githubusercontent.com/KunaalNaik/AltUni_DataScience_Python_Basics/main/R_Course/Employee_Attrition.csv")
+
+# Check First few rows
+head(data)
+
+# Summary Statistics
+summary(data)
+
+dfSummary(data)
+
+# Frequency
+freq(data$BusinessTravel)
+
+# pandas = dplyr
+# Highest Attrition by Department
+# %>% - pipe operator
+summary_department <- data %>%
+  group_by(Department) %>%
+  summarise(sum_attrition = sum(Attrition))
+
+print(summary_department)
+
+
+
+
+
+
