42eb65b0ffba36f285799f388505013d762af723
Kunaal Naik <33804703+KunaalNaik@users.noreply.github.com>
2023-10-14 20:31:43 +0530

    final

--- R_Course/TimeSeries/Forecasting.R
+++ R_Course/TimeSeries/Forecasting.R
@@ -18,3 +18,23 @@ ggplot(data, aes(x = Yr_Qtr, y = Sales)) + geom_line(group = 1) + geom_point()
 
 ## Decomposition
 decomposed <- stl(data_ts, s.window = "periodic")
+
+plot(decomposed)
+
+# Extract the individual components
+seasonal <- decomposed$time.series[, "seasonal"]
+trend <- decomposed$time.series[, "trend"]
+remainder <- decomposed$time.series[, "remainder"]
+
+# 2/ Forecast - Multiplicative Model
+
+ets_fit <- ets(data_ts, model = "MAM")
+
+est_forecast <- forecast(ets_fit, h = 4)
+
+# Plot the forecast
+plot(est_forecast)
+
+
+
+
