261f352fae4b6bcc77dbc909af1354c26c995d4a
Kunaal Naik <33804703+KunaalNaik@users.noreply.github.com>
2023-10-15 19:52:37 +0530

    chi sq

--- R_Course/R1_Basics_gemstone_Stats.R
+++ R_Course/R1_Basics_gemstone_Stats.R
@@ -18,13 +18,37 @@ head(data)
 # Summary Statistics
 summary(data)
 
-## Correlation
+## Correlation - with Price - Numerical and Numerical
 cor_mar = cor(data %>% select(-c(id, cut,color, clarity)), method = "pearson")
+cor_with_price <- cor_mar[,"price"]
+
+print(cor_with_price)
+
+## ANOVA- Categorical and Numerical
+
+# cut & price
+anova_result <- aov(price ~ cut, data = data)
+
+summary(anova_result)
 
 
+## T-Test - Numerical & Numerical
+ideal <- data[data$cut == "Ideal", "price"]
+premium <- data[data$cut == "Premium", "price"]
 
+t.test(ideal,premium)
 
+## Chi Sq Data - Categorical and Categorical
+#Data Import
+attrition_data <- read.csv("https://raw.githubusercontent.com/KunaalNaik/AltUni_DataScience_Python_Basics/main/R_Course/Employee_Attrition.csv")
 
+# Create a Contingency Table
+contingency_table <- table(attrition_data$Attrition, attrition_data$Department)
 
+chi_sq_test <- chisq.test(contingency_table)
+
+print(chi_sq_test)
+
+#  Education Field and Attrition
 
 
